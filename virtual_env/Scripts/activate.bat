c01073296d1411f9aa6d4c1c444a87f8982da106
Agon Adili <agon.adili@gmail.com>
2024-03-02 15:31:39 +0100

    URLs fixed

--- virtual_env/Scripts/activate.bat
+++ virtual_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Agon\Desktop\Django App\virtual_env
+set VIRTUAL_ENV=C:\Users\Agon\Desktop\Django App\PROJEKTI\virtual_env
 
 if not defined PROMPT set PROMPT=$P$G
 
